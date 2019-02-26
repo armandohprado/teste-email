f031af85254cc94891b8776f014500ede1a1aaf5
armando <armandohprado2801@gmail.com>
2019-02-26 18:58:07 -0300

    api-giphy

---
+++ angularJS/projects/site-institucional/api-giphy/src/app/app.module.ts
@@ -0,0 +1,17 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { GiphySearchManualComponent } from './giphy/giphy-search-manual/giphy-search-manual.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,GiphySearchManualComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ angularJS/projects/site-institucional/api-giphy/src/app/giphy/giphy-search-manual/giphy-search-manual.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector:    'appgiphy-search-manual',
+    templateUrl: 'giphy-search-manual.component.html',
+    moduleId: module.id
+  })
+  export class GiphySearchManualComponent implements OnInit {
+    constructor() {
+        
+    }
+    ngOnInit() {
+        
+    }
+  }
